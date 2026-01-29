995c7bac3b9733dffda09af24db209c81b187320
shinyEazy <tanhngusi39@gmail.com>
2026-01-30 03:26:59 +0700

    feat: Add Lunar New Year theme and replace Christmas theme in the application.

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import { Router } from './routes'
 import TeacherDayTheme from './components/themes/teacher-day'
 import ChristmasTheme from './components/themes/christmas'
+import LunarNewYearTheme from './components/themes/lunarnewyear'
 import { AdminAuthProvider } from './hooks/useAdminAuth'
 
 function App() {
 	return (
 		<AdminAuthProvider>
-			<ChristmasTheme />
+			<LunarNewYearTheme />
 			<Router />
 		</AdminAuthProvider>
 	)
